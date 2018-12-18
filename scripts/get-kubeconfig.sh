24d0d1f88e46395c9aaeb886ea36663170d1a63a
Todd Thomas <todd.thomas@panasonic.aero>
2018-12-17 16:44:51 -0800

    get-kubeconfig.sh now pulls the binary if it's not installed on the system

--- scripts/get-kubeconfig.sh
+++ scripts/get-kubeconfig.sh
@@ -24,6 +24,13 @@
 ###----------------------------------------------------------------------------
 #"${1?  Wheres my first agument, bro!}"
 # ENV Stuff
+awsAuthVers='1.11.5'
+awsAuthDate='2018-12-06'
+tmpAuthBin='/tmp/aws-iam-authenticator'
+sysAuthBin='/usr/local/bin/aws-iam-authenticator'
+tmpAuthSHA='/tmp/aws-iam-authenticator.sha256'
+remoteAuthBin="https://amazon-eks.s3-us-west-2.amazonaws.com/${awsAuthVers}/${awsAuthDate}/bin/darwin/amd64/aws-iam-authenticator"
+remoteAuthSHA="https://amazon-eks.s3-us-west-2.amazonaws.com/${awsAuthVers}/${awsAuthDate}/bin/darwin/amd64/aws-iam-authenticator.sha256"
 iamAuthURL='https://docs.aws.amazon.com/eks/latest/userguide/configure-kubectl.html'
 # Data
 
@@ -41,34 +48,56 @@ function pMsg() {
 ###----------------------------------------------------------------------------
 ### Check basic requirements
 ###---
-if ! type -P aws-iam-authenticator; then
-    pMsg "The AWS Authenticator is not installed; I'm out!"
+if ! type -P "${sysAuthBin##*/}"; then
+    pMsg "The AWS Authenticator is not installed; pulling it..."
     pMsg "Instructions: $iamAuthURL"
-    exit 1
+    curl -so "$tmpAuthBin" "$remoteAuthBin"
+    pMsg "Validating the bin with the SHA-256 sum..."
+    curl -so "$tmpAuthSHA" "$remoteAuthSHA"
+    shaTarget="$(cut -d' ' -f1 "$tmpAuthSHA")"
+    shaActual="$(openssl sha -sha256 "$tmpAuthBin")"
+    if [[ "${shaActual##*\ }" != "$shaTarget" ]]; then
+        pMsg "The download flaked; check the instructions above; I'm out."
+        exit 1
+    else
+        pMsg "The download was good, moving it home..."
+        mv "$tmpAuthBin" "$sysAuthBin"
+        chmod u+x "$sysAuthBin"
+    fi
 fi
 
 
-
-
 ###---
-###
+### Display the actual credentials
 ###---
-pMsg "We'll attempt to get the EKS config with these credentials:"
-aws sts get-caller-identity
+#pMsg """
+#
+#    We'll attempt to get the EKS config with these credentials:
+#
+#    """
+#aws sts get-caller-identity
 
 
 ###---
 ### Update the local KUBECONFIG with the new, remote cluster details
 ### REF: https://docs.aws.amazon.com/eks/latest/userguide/create-kubeconfig.html
 ###---
-pMsg "Updating local ${KUBECONFIG##*.}..."
+pMsg """
+
+    Updating local KUBECONFIG...
+
+    """
 aws eks update-kubeconfig --name "$TF_VAR_cluster_name"
 
 
 ###---
 ### Change the Cluster Name locally
 ###---
-pMsg "Changing that obnoxious name..."
+pMsg """
+
+    Changing that obnoxious name...
+
+    """
 kubectl config rename-context \
 	"arn:aws:eks:${TF_VAR_region}:405322537961:cluster/${TF_VAR_cluster_name}" \
 	"$TF_VAR_cluster_name"
@@ -77,7 +106,11 @@ kubectl config rename-context \
 ###---
 ### REQ
 ###---
-pMsg "The new contexts:"
+pMsg """
+
+    The new contexts:
+
+    """
 kubectl config get-contexts
 kubectl config use-context  "$TF_VAR_cluster_name"
 
@@ -85,7 +118,11 @@ kubectl config use-context  "$TF_VAR_cluster_name"
 ###---
 ### REQ
 ###---
-pMsg "The new EKS Cluster:"
+pMsg """
+
+    The new EKS Cluster:
+
+    """
 kubectl cluster-info
 
 
