01fb33105c49655a568f739a1bd2c12e57547134
Todd Thomas <todd.thomas@panasonic.aero>
2018-12-21 09:28:39 -0800

    there is an AZ count; we'll no longer have to assign a number

--- mods/kubes/networking.tf
+++ mods/kubes/networking.tf
@@ -25,7 +25,7 @@ resource "aws_vpc" "kubes" {
 
 # Create Subnetes within the VPC
 resource "aws_subnet" "kubes" {
-  count = 3
+  count = "${length(data.aws_availability_zones.available.names)}"
 
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   cidr_block        = "10.0.${count.index}.0/24"
